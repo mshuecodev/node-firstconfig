1bb3b7071d9ab28ba81d3ab58e8fa1ebc48746d2
misna04 <misnasyari@gmail.com>
2024-07-22 19:49:46 +0800

    crud config fixed

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -9,6 +9,15 @@ exports.getBlogs = async (req, res) => {
 	}
 }
 
+exports.getBlogByID = async (req, res) => {
+	try {
+		const result = await Blog.findById(req.params.id)
+		if (!result) return res.status(404).json({ message: "Data not found" })
+	} catch (err) {
+		res.status(500).json({ message: err.message })
+	}
+}
+
 exports.createBlog = async (req, res) => {
 	try {
 		const data = new Blog({ name, email, password })
@@ -16,5 +25,25 @@ exports.createBlog = async (req, res) => {
 		res.status(201).json(data)
 	} catch (err) {
 		res.status(400).json({ message: err.message })
+	}
+}
+
+exports.updateBlog = async (req, res) => {
+	try {
+		const result = await Blog.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
+		if (!result) return res.status(404).json({ message: "Data not found" })
+		res.status(200).json(result)
+	} catch (error) {
+		res.status(400).json({ message: error.message })
+	}
+}
+
+exports.deleteBlog = async (req, res) => {
+	try {
+		const result = await Blog.findByIdAndDelete(req.params.id)
+		if (!result) return res.status(404).json({ message: "Data not found" })
+		res.status(200).json({ message: "Data deleted" })
+	} catch (error) {
+		res.status(500).json({ message: error.message })
 	}
 }
--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,8 +1,11 @@
 const express = require("express")
-const { getBlogs, createBlog } = require("../controllers/blogController")
+const { getBlogs, createBlog, getBlogByID, updateBlog, deleteBlog } = require("../controllers/blogController")
 const router = express.Router()
 
 router.get("/", getBlogs)
+router.get("/:id", getBlogByID)
 router.post("/", createBlog)
+router.put("/:id", updateBlog)
+router.delete("/:id", deleteBlog)
 
 module.exports = router
